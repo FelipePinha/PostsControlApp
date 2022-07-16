4b5a46929cd394da38f9f5e8be6ac4ec8246e3c3
Felipe Pinha <felipe_pc7@hotmail.com>
2022-07-16 15:56:44 -0300

    base structure

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Wrapper } from "./App.styles";
+
+export const App = () => {
+  return(
+    <Wrapper>
+      todo: app
+    </Wrapper>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Home } from './views/Home/Home.view'
+import './global.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>
+)
