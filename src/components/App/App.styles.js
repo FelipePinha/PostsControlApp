159675404ad300c1cf7a0cdedf16b6c4b32acd35
Felipe Pinha <felipe_pc7@hotmail.com>
2022-07-17 00:23:09 -0300

    added base logic

--- src/components/App/App.styles.js
+++ src/components/App/App.styles.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
     width: 100vw;
     height: 100vh;
+    overflow-x: hidden;
 
     display: flex;
     flex-direction: column;
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,17 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Wrapper } from "./Form.styles";
 import { Post } from "../Post/Post";
 
 export const Form = () => {
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+    const [posts, setPosts] = useState([])
+
+    const handleAddPost = () => {
+        if(title && content) {
+            const newPosts = [...posts, {
+                title: title,
+                content: content
+            }]
+
+            setPosts(newPosts)
+            setTitle('')
+            setContent('')
+        }
+
+       
+    }
+
     return (
         <Wrapper>
-            <input type="text" placeholder="Título"/>
-            <textarea maxLength={100} type="text" placeholder="O que está pensando?"></textarea>
+            <input 
+             onChange={e => setTitle(e.target.value)}
+             type="text" 
+             placeholder="Título"
+             />
+            
+            <textarea 
+             onChange={e => setContent(e.target.value)}
+             maxLength={100}
+             type="text" 
+             placeholder="O que está pensando?">
+             </textarea>
 
-            <button>Enviar</button>
+            <button onClick={handleAddPost}>Enviar</button>
 
             <h1>Posts</h1>
-            <Post title={'Title'} content={'Test Content'}/>
+            {
+                posts.map(post => (
+                    <Post title={post.title} content={post.content}/>
+                ))
+            }
         </Wrapper>
     )
 }
