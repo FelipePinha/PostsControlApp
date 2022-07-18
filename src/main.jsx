b5c04fdd961267c93bae589508e2461e8df72c64
Felipe Pinha <felipe_pc7@hotmail.com>
2022-07-18 15:35:10 -0300

    imported store

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Home } from './views/Home/Home.view'
+import store from '../reducers/store'
+import { Provider } from 'react-redux'
 import './global.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <Provider store={store}>
+      <Home />
+    </Provider>
   </React.StrictMode>
 )
